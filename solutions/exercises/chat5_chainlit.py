d5a8e1438e90269efe50cd0cbff7da89b38035b1
usmanwaz <u_double_u@hotmail.com>
2025-04-24 04:14:26 +0000

    Your commit message here

--- solutions/exercises/chat5_chainlit.py
+++ solutions/exercises/chat5_chainlit.py
@@ -2,9 +2,6 @@ from ctransformers import AutoModelForCausalLM
 from typing import List
 import chainlit as cl
 
-llm = AutoModelForCausalLM.from_pretrained(
-    "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
-)
 
 '''
 If using Llama2 ...
@@ -15,20 +12,43 @@ llm = AutoModelForCausalLM.from_pretrained(
 '''
 
 
-def get_prompt(instruction: str, history: List[str] = None) -> str:
+def get_prompt(instruction: str, history: List[str]) -> str:
     system = "You are an AI assistant that gives helpful answers. You answer the question in a short and concise way."
     prompt = f"### System:\n{system}\n\n### User:\n"
-    if history is not None:
+    if len(history) > 0:
         prompt += f"This is the conversation history so far: {''.join(history)} Now answer the question: "
     prompt += f"{instruction}\n\n### Response:\n"
-    print(prompt)
+    # print(prompt)
     return prompt
+
+
+@cl.on_chat_start
+def on_chat_start():
+    cl.user_session.set("message_history", [])
+    global llm
+    llm = AutoModelForCausalLM.from_pretrained(
+        "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
+    )
 
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    response = f"Hello, you just sent: {message.content}!"
-    await cl.Message(response).send()
+    message_history = cl.user_session.get("message_history")
+
+    msg = cl.Message(content="")
+    await msg.send()
+
+    prompt = get_prompt(message.content, message_history)
+    response = ""
+    for word in llm(prompt, stream=True):
+        await msg.stream_token(word)
+        response += word
+
+    # response = llm(prompt)
+    # response = f"Hello, you just sent: {message.content}!"
+    await msg.update()
+    message_history.append(response)
+    # await cl.Message(response).send()
 
 '''
 
